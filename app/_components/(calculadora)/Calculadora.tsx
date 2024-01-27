2b5937d3922714f8df7362cb4129d932d699f3b8
emers0mn <emeron0000@gmail.com>
2024-01-27 16:03:37 -0300

    atualização de css

--- app/_components/(calculadora)/Calculadora.tsx
+++ app/_components/(calculadora)/Calculadora.tsx
@@ -1,57 +1,59 @@
- 'use client';
+'use client';
 
- import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import style from './calculadora.module.css'
 
- type PesoType = {
+type Repositorio = {
     totalBid: number;
-  };
+};
 
- export default function Calculadora() {
+export default function Calculadora() {
 
-    const [peso, setPeso] = useState<PesoType>({ totalBid: 0 });
+    const [peso, setPeso] = useState<Repositorio>({ totalBid: 0 });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('https://criptoya.com/api/lemoncash/usdt/ars/0.1');
-      const resposta = await res.json();
-      console.log(resposta);
-      setPeso(resposta);
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('https://criptoya.com/api/lemoncash/usdt/ars/0.1');
+            const resposta = await res.json();
+            console.log(resposta);
+            setPeso(resposta);
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  const [reais, setReais] = useState<PesoType>({ totalBid: 0 });
+    const [reais, setReais] = useState<Repositorio>({ totalBid: 0 });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('https://criptoya.com/api/binance/usdt/brl/0.1');
-      const resposta = await res.json();
-      console.log(resposta);
-      setReais(resposta);
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('https://criptoya.com/api/binance/usdt/brl/0.1');
+            const resposta = await res.json();
+            console.log(resposta);
+            setReais(resposta);
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
+    const [valor, setValor] = useState(0);
 
-
-
-     const [valor, setValor] = useState(0);
-
-     function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
         setValor(Number(event.target.value));
-      }
+    }
 
-     return (
-         <section>
-             <h2>Calculadora:</h2>
-             <form>
-                 <input placeholder="Reais para pesos..." value={valor} onChange={handleInputChange}/>
-             </form>
+    return (
+        <section className={style.content}>
+            <h2>Calculadora:</h2>
+            <form className={style.contentInput}>
+                <label>Reais:</label>
+                <div className={style.cal}>
+                    <small>R$</small>
+                    <input placeholder="Reais para pesos..." value={valor} onChange={handleInputChange} />
+                </div>
+            </form>
 
-             <small>Resultado: {(valor * (Math.ceil(peso.totalBid / reais.totalBid))).toLocaleString('pt-BR')}</small>
-            
-         </section>
-     );
- }
+            <strong>Resultado: ${(valor * (Math.ceil(peso.totalBid / reais.totalBid))).toLocaleString('pt-BR')}</strong>
+
+        </section>
+    );
+}
